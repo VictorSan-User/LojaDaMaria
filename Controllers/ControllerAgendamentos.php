525c4374035f15b70318f72d2eab95e440e1df11
Victor <victorhenriquesan@outlook.com>
2025-05-24 23:39:59 -0300

    Criado Objeto Compromisso e alguns Metodos

---
+++ Controllers/ControllerAgendamentos.php
@@ -0,0 +1,91 @@
+<?php
+
+class Compromisso{
+    public $data_inicio;
+    public $data_fim;
+    public $titulo;
+    public $descricao;
+    public $cliente;
+
+    public $connect;
+
+    public function __construct($connect, $cliente, $descricao, $titulo, $data_fim, $data_inicio)
+    {
+        $this->connect = $connect;
+        $this->cliente = $cliente;
+        $this->descricao = $descricao;
+        $this->titulo = $titulo;
+        $this->data_fim = $data_fim;
+        $this->data_inicio = $data_inicio;
+    }
+
+    public function salvar(){
+        $dados = $this->connect->prepare("INSERT INTO TB_AGENDAMENTOS(data_inicial, data_final, titulo, descricao, cliente) VALUES(?, ?, ?, ?, ?)");
+
+        if($dados){
+            $dados->bind_param("sssss", $this->data_inicio, $this->data_fim, $this->titulo, $this->descricao, $this->cliente);
+
+            if($dados->execute()){
+                return ['success' => true, 'message' => 'Agendamento salvo!'];
+            }else{
+                return "Erro ao salvar ". $dados->error;
+            }
+        }
+    }
+    public function editar($id){
+        $editContent = $this->connect->prepare("UPDATE TB_AGENDAMENTOS SET data_inicial=?, data_final=?, titulo=?, descricao=?, cliente=? WHERE id=?");
+        if($editContent){
+            $editContent->bind_param("sssssi", $this->data_inicio, $this->data_fim, $this->titulo, $this->descricao, $this->cliente, $id);
+            if($editContent->execute()){
+                return ['success' => true, 'message' => 'Agendamento Atualizado!'];
+            } else {
+                return "Erro ao atualizar: " . $editContent->error;
+            }
+        } else {
+            return "Erro na preparação: " . $this->connect->error;
+        }
+    }
+    public function excluir($id) {
+        $stmt = $this->connect->prepare("DELETE FROM TB_AGENDAMENTOS WHERE id = ?");
+        if($stmt){
+            $stmt->bind_param("i", $id);
+            if($stmt->execute()){
+                return ['success' => true, 'message' => 'Compromisso Excluido!'];
+
+            } else {
+                return "Erro ao excluir: " . $stmt->error;
+            }
+        } else {
+            return "Erro na preparação: " . $this->connect->error;
+        }
+    }
+    public static function listarFuturos($connect) {
+    $hoje = date('Y-m-d');
+    $stmt = $connect->prepare("SELECT * FROM TB_AGENDAMENTOS WHERE data_inicial >= ? ORDER BY data_inicial ASC");
+    $stmt->bind_param("s", $hoje);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $agendamentos = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $agendamentos[] = $row;
+    }
+    return $agendamentos;
+    }
+
+    public static function listarPassados($connect) {
+        $hoje = date('Y-m-d');
+        $date = $connect->prepare("SELECT * FROM TB_AGENDAMENTOS WHERE data_final < ? ORDER BY data_inicial DESC");
+        $date->bind_param("s", $hoje);
+        $date->execute();
+        $result = $date->get_result();
+
+        $agendamentos = [];
+        while ($row = $result->fetch_assoc()) {
+            $agendamentos[] = $row;
+        }
+        return $agendamentos;
+    }
+
+}
